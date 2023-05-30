9667d1c5834e1a8e9b37e54756d9339d1ae92ed8
Leonce Nshuti <lnshuti@Leonces-Mac-mini.local>
2023-05-29 21:35:21 -0500

    add borrowed components from openbb

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
 import streamlit as st 
 
 def main():
+    st.set_page_config(layout="wide")
+    st.title('**Healthcare Quality Measures** between hospitals and across states in the United States')
 
-     st.set_page_config(layout="wide")
-     st.title('**Healthcare Quality Measures** between hospitals and accross states in the United States')
-
-     st.write(
-          '''We track the following six measures of healthcare quality : 
-
-                1. Mortality - Death rate for heart attack patients
-
-                2. Safety of Care - Serious complications
-
-                3. Readmission - 30-day readmission rate for heart attack patients
-
-                4. Patient Experience - Patient experience survey results
-
-                5. Effectiveness of Care - Timeliness of care for heart attack patients
-
-                6. Timeliness of Care - Timeliness of care for heart attack patients
+    st.write(
         '''
-            )
+        We track the following six measures of healthcare quality : 
 
+        1. Mortality - Death rate for heart attack patients
+        2. Safety of Care - Serious complications
+        3. Readmission - 30-day readmission rate for heart attack patients
+        4. Patient Experience - Patient experience survey results
+        5. Effectiveness of Care - Timeliness of care for heart attack patients
+        6. Timeliness of Care - Timeliness of care for heart attack patients
+        '''
+    )
 
-main()
+if __name__ == "__main__":
+    main()
